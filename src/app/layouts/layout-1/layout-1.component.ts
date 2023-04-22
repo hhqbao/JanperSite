5ba1674341bafb8aade763f9f6b8c70e8da51c66
jason <hhqbao@gmail.com>
2023-04-22 22:10:48 +1000

    Fixed left nav menu

--- src/app/layouts/layout-1/layout-1.component.ts
+++ src/app/layouts/layout-1/layout-1.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { LeftNavMenuComponent } from 'src/app/components/left-nav-menu/left-nav-menu.component';
 
 @Component({
   selector: 'app-layout-1',
   templateUrl: 'layout-1.component.html',
 })
 export class Layout1Component implements OnInit {
+  @ViewChild('leftNavMenu') leftNavMenu!: LeftNavMenuComponent;
+
   constructor() {}
 
   ngOnInit() {}
